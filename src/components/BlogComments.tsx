6860aac9a8b6703f8f25b60bd2ccbdac16fe25bf
AI Tool HQ <aitoolhq@users.noreply.github.com>
2026-04-16 22:39:19 +0000

    Restore Giscus comments

---
+++ src/components/BlogComments.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export function BlogComments() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://giscus.app/client.js';
+    script.async = true;
+    script.crossOrigin = 'anonymous';
+    script.setAttribute('data-repo', 'aitoolhq/aitoolhq');
+    script.setAttribute('data-repo-id', 'R_kgDOL8-9ZQ');
+    script.setAttribute('data-category', 'AIToolHQ Blog Posts');
+    script.setAttribute('data-category-id', 'DIC_kwDOL8-9Zc4CeAgR');
+    script.setAttribute('data-mapping', 'pathname');
+    script.setAttribute('data-strict', '0');
+    script.setAttribute('data-reactions-enabled', '1');
+    script.setAttribute('data-emit-metadata', '0');
+    script.setAttribute('data-input-position', 'bottom');
+    script.setAttribute('data-theme', 'dark_dimmed');
+    script.setAttribute('data-lang', 'en');
+
+    const commentsDiv = document.getElementById('giscus-comments');
+    if (commentsDiv) {
+      commentsDiv.appendChild(script);
+    }
+
+    return () => {
+      const existingScript = document.querySelector('script[src="https://giscus.app/client.js"]');
+      if (existingScript) {
+        existingScript.remove();
+      }
+    };
+  }, []);
+
+  return (
+    <div id="giscus-comments" style={{ marginTop: '3rem' }}>
+      {/* Giscus comments will load here */}
+    </div>
+  );
+}
